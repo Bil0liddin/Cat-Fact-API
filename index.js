ae6f47856b158c5b54711231847f34ff345b78b6
Bil0liddin <138793470+Bil0liddin@users.noreply.github.com>
2023-07-26 16:56:35 +0500

    change

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 async function fact() {
     let data = await fetch('https://catfact.ninja/fact');
     let info = await data.json();
-    document.getElementById("fact").innerHTML =
-      "Fact about cats: " + info.fact;
+    document.getElementById("fact").innerHTML =  info.fact;
   }
   
